e26fcdecae54cdad967731674c593174840e0bd1
Cory Spicer <corys4139@gmail.com>
2020-07-13 05:55:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,12 +9,14 @@ def display_board(blank_board)
   puts "   |   |   "
 end
 
-board1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-  puts (rows[0]).to eq("   |   |   ")
-  puts (rows[1]).to eq("-----------")
-  puts (rows[2]).to eq("   | X |   ")
-  puts (rows[3]).to eq("-----------")
-  puts (rows[4]).to eq("   |   |   ")
+def board1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+  
 board2 = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
 board3 = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 board4 = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
