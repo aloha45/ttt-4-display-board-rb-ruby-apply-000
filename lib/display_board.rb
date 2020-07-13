23f0e1784801b991e22d451cbdaee17e21981199
Cory Spicer <corys4139@gmail.com>
2020-07-13 05:53:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,6 +10,11 @@ def display_board(blank_board)
 end
 
 board1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("   | X |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("   |   |   ")
 board2 = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
 board3 = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 board4 = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
